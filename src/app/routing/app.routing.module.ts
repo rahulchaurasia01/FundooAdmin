ff3931bb0fa1026037205496222a80e27a1986b5
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-03-13 17:06:07 +0530

    Applied Guard to app routing for dashboard component

--- src/app/routing/app.routing.module.ts
+++ src/app/routing/app.routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { AuthGuard } from '../services/authguard/auth.guard';
 import { LoginComponent } from '../components/login/login.component';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
 
 
 const routes: Routes = [
 
-  { path: '', redirectTo: "login", pathMatch: "full" },
-  { path: "login", component: LoginComponent }
+  { path: '', redirectTo: "/login", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "dashboard", component: DashboardComponent, canActivate: [AuthGuard] }
 
 ]
 
